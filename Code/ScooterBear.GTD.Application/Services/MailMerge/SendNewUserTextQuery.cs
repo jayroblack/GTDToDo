5e28535763008378b6766d7e339f07bccdd16acf
James Rhodes <james.rhodes@tigerpistol.com>
2020-01-30 03:17:33 -0600

    Done for tonight

---
+++ Code/ScooterBear.GTD.Application/Services/MailMerge/SendNewUserTextQuery.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScooterBear.GTD.Application.Services.MailMerge
+{
+    class SendNewUserTextQuery
+    {
+    }
+}
